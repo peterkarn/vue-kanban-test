c45a6bcd9739fb1c75c930e854edd582d6fabb3a
peterkarnaukhsite <62539689+peterkarnaukhsite@users.noreply.github.com>
2021-08-14 13:44:33 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="page-header">
+      <h1>Kanban Board</h1>
+      <p class="lead">An example of a Kanban board, written in Vue.js</p>
+    </div>
+    <menu-bar></menu-bar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MenuBar from '@/components/MenuBar';
+
+  export default {
+    name: 'app',
+    components: {
+      'menu-bar': MenuBar
+    }
+  };
+</script>
+
+<style>
+  .page-header h1 {
+    font-weight: 300;
+  }
+  .card {
+    margin-bottom: 10px;
+  }
+  .card:last-child {
+    margin-bottom: 25px;
+  }
+  .card-block {
+    padding: 15px;
+  }
+</style>
